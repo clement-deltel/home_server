a3b16a4f7a53bab9251bcfc8a3e159948e4a8f3b
Clement Deltel <clement.deltel@proton.me>
2023-09-27 11:25:27 -0500

    fix: specify server_home

--- ansible/install.sh
+++ ansible/install.sh
@@ -1,4 +1,4 @@
-sudo apt update -y
+SERVER_HOME='/opt/home-server'
 sudo apt install -y software-properties-common
 sudo add-apt-repository -y --update ppa:ansible/ansible
 sudo apt update -y
@@ -6,5 +6,6 @@ sudo apt install -y ansible
 
 # Possible tags: init, update, docker
 
-ansible-playbook -u ubuntu --ask-become-pass playbooks/ubuntu.yml --tags init
-ansible-playbook -u docker --ask-become-pass playbooks/docker.yml --tags docker
+ansible-playbook -u ubuntu --ask-become-pass playbooks/ubuntu.yml --extra-vars server_home=${SERVER_HOME} --tags init
+
+ansible-playbook -u docker --ask-become-pass playbooks/docker.yml --extra-vars server_home=${SERVER_HOME} --tags docker
